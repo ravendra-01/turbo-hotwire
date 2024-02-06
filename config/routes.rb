6d7c885106550f7df4c3bb43a5aefac6b6b4e199
rkpatel <rk.ravendra01@gmail.com>
2024-02-06 10:24:22 +0530

    implemented member show api with view page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  
+  get 'member/:id', to: 'members#show', as: 'member'
 end
