3a81f76b2914dfef70dacc97e932fae2030bed0f
rkpatel <rk.ravendra01@gmail.com>
2024-02-06 09:21:49 +0530

    added index view page and implemented search functionality using ransack

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,25 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable
+  
+  PROFILE_TITLE = [
+    'Senior Ruby on Rails Developer',
+    'Full Stack Ruby on Rails Developer',
+    'Senior Full Stack Ruby on Rails Developer',
+    'Junior Full Stack Ruby on Rails Developer',
+    'Senior Java Developer',
+    'Senior Front End Developer'
+  ].freeze
+
+  def name
+    "#{first_name} #{last_name}".strip
+  end
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["city", "country"]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    []
+  end
 end
