f1df09757b55a215e46bd0419ee598b8624e699b
rkpatel <rk.ravendra01@gmail.com>
2024-02-07 23:43:03 +0530

    added column for connected user ids and updated mutual connections count in connected member list

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -3,6 +3,7 @@ class MembersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @connections = Connection.where('user_id = ? OR connected_user_id = ?', params[:id], params[:id]).where(status: "accepted")
+    @mutual_connection = current_user.connected_user_ids.intersection(@user.connected_user_ids)
   end
 
   def edit_description; end
@@ -26,9 +27,9 @@ class MembersController < ApplicationController
   end
 
   def connections
-    @requested_connections = Connection.includes(:requested).where(user_id: params[:id], status: "accepted")
-    @received_connections = Connection.includes(:received).where(connected_user_id: params[:id], status: "accepted")
-    @total_connections = @requested_connections.count + @received_connections.count
+    @user = User.find(params[:id])
+    @connected_users = User.where(id: @user.connected_user_ids)
+    @total_connections = @connected_users.count
   end
 
   private
